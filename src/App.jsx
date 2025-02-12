07dd07784f7bb11395a30d9ff079d2222ec18d22
Saikat Kar <ksaikat009@gmail.com>
2025-02-12 22:22:52 +0530

    new

---
+++ src/App.jsx
@@ -0,0 +1,89 @@
+import { useState } from "react";
+import lovesvg from "./assets/cat-talking.gif";
+import lovesvg2 from "./assets/happy.gif";
+
+export default function Page() {
+  const [noCount, setNoCount] = useState(0);
+  const [yesPressed, setYesPressed] = useState(false);
+  const yesButtonSize = noCount * 20 + 16;
+
+  const handleNoClick = () => {
+    setNoCount(noCount + 1);
+  };
+
+  const getNoButtonText = () => {
+    const phrases = [
+      "No",
+      "Are you sure?",
+      "Really sure?",
+      "Think again!",
+      "Last chance!",
+      "Surely not?",
+      "You might regret this!",
+      "Give it another thought!",
+      "Are you absolutely certain?",
+      "Jungkook ki kasam say yes!",
+      "Have a heart!",
+      "Don't be so cold!",
+      "Change of heart?",
+      "Wouldn't you reconsider?",
+      "ab Abheree didi mil gayi to mujhse yapping nahi?",
+      "You're breaking my heart ,Punam ;(",
+      "Is that your final answer?",
+      "You're breaking my heart ;(",
+      "Plsss Punam? :( You're breaking my heart",
+    ];
+
+    return phrases[Math.min(noCount, phrases.length - 1)];
+  };
+
+  return (
+    <div className="overflow-hidden flex flex-col items-center justify-center pt-4 h-screen -mt-16 selection:bg-rose-600 selection:text-white text-zinc-900">
+      {yesPressed ? (
+        <>
+          <img src={lovesvg2}/>
+          <div className="text-4xl md:text-6xl font-bold my-4">
+            Ok Yayyyyy!!!
+          </div>
+        </>
+      ) : (
+        <>
+          <img
+            src={lovesvg}
+          />
+          <h1 className="text-4xl md:text-6xl my-4 text-center">
+            Will you keep yapping to me forever?
+          </h1>
+          <div className="flex flex-wrap justify-center gap-2 items-center">
+            <button
+              className={`bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded-lg mr-4`}
+              style={{ fontSize: yesButtonSize }}
+              onClick={() => setYesPressed(true)}
+            >
+              Yes
+            </button>
+            <button
+              onClick={handleNoClick}
+              className=" bg-rose-500 hover:bg-rose-600 rounded-lg text-white font-bold py-2 px-4"
+            >
+              {noCount === 0 ? "No" : getNoButtonText()}
+            </button>
+          </div>
+        </>
+      )}
+      <Footer />
+    </div>
+  );
+}
+
+const Footer = () => {
+  return (
+    <a
+      className="fixed bottom-2 right-2 backdrop-blur-md opacity-80 hover:opacity-95 border p-1 rounded border-rose-300"
+      href="https://github.com/Xeven777/valentine"
+      target="__blank"
+    >
+      
+    </a>
+  );
+};
